6c32bb8d9370d676cc161c0e500f8354ae719fc6
Sarah-C-F <johndoe@example.com>
2022-11-02 17:23:32 -0700

    added files

---
+++ src/main/java/dev/farrens/PathfinderCS/PathfinderCsApplication.java
@@ -0,0 +1,13 @@
+package dev.farrens.PathfinderCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathfinderCsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/farrens/PathfinderCS/PathfinderCsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.farrens.PathfinderCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderCsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
